a0f9a10691c1519002d7b277957796e850b1dc62
sakshipatil16 <sakshi16.patil2000@gmail.com>
2020-12-23 20:54:08 +0530

    added join room amd create room page

--- server.js
+++ server.js
@@ -16,6 +16,20 @@ app.get('/', (req, res) => {
   })
 
 
+  app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+  })
+  
+  io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+      socket.join(roomId)
+      socket.to(roomId).broadcast.emit('user-connected', userId)
+  
+      socket.on('disconnect', () => {
+        socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      })
+    })
+  })
   
 
 server.listen(3000)
