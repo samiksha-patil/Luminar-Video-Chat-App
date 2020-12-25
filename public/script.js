792ed6b7cc8d674be1fd3b2c30dc88122914243e
sakshipatil16 <sakshi16.patil2000@gmail.com>
2020-12-25 23:45:05 +0530

    added video call

---
+++ public/script.js
@@ -0,0 +1,35 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer()
+const myVideo = document.createElement('video')
+
+myVideo.muted = true
+const peers = {}
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  myVideoStream = stream;
+  addVideoStream(myVideo, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id,NAME)
+})
