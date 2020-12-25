8173624d7429d562840e763b5efb1204709092d0
samiksha-patil <samiksha16.patil@gmail.com>
2020-12-25 23:59:06 +0530

    added mute button

--- public/script.js
+++ public/script.js
@@ -31,7 +31,7 @@ socket.on('user-disconnected', userId => {
 })
 
 myPeer.on('open', id => {
-    
+
   socket.emit('join-room', ROOM_ID, id,NAME)
 
 })
@@ -56,3 +56,31 @@ function addVideoStream(video, stream) {
   })
   videoGrid.append(video)
 }
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getAudioTracks()[0].enabled = false;
+      setUnmuteButton();
+    } else {
+      setMuteButton();
+      myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+  };
+  
+  const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+     
+    `;
+    document.querySelector(".main__mute_button").innerHTML = html;
+  };
+  
+  const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+     
+    `;
+    document.querySelector(".main__mute_button").innerHTML = html;
+  };
+  
