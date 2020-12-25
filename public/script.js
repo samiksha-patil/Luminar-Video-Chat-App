dbce37e0e9b7ce7807f6d26cdcb6856b1a5284b0
sakshipatil16 <sakshi16.patil2000@gmail.com>
2020-12-26 00:02:16 +0530

    added stop video button

--- public/script.js
+++ public/script.js
@@ -83,4 +83,31 @@ const muteUnmute = () => {
     `;
     document.querySelector(".main__mute_button").innerHTML = html;
   };
+
+
+  const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+      myVideoStream.getVideoTracks()[0].enabled = false;
+      setPlayVideo();
+    } else {
+      setStopVideo();
+      myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+  };
   
+  const setStopVideo = () => {
+    const html = `
+      <i class="fas fa-video"></i>
+     
+    `;
+    document.querySelector(".main__video_button").innerHTML = html;
+  };
+  
+  const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      
+    `;
+    document.querySelector(".main__video_button").innerHTML = html;
+  };
