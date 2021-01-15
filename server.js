de344260ff0f0227b9c34d5bfaa259eaa21ab0eb
sakshipatil16 <sakshi16.patil2000@gmail.com>
2021-01-15 17:25:54 +0530

    added participant's view

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+//const { v1: uuidV1 } = require('uuid')
 
+const users= []
 
 const publicDirectoryPath = path.join(__dirname, 'public')
 app.use(express.static(publicDirectoryPath))
@@ -15,17 +17,33 @@ app.get('/', (req, res) => {
   res.render('index.ejs')
   })
 
+
+  app.get('/abc', (req, res) => {
+    res.sendFile(publicDirectoryPath+'//board.html')
+    })
+
+
   app.get('/:name/:room', (req, res) => {
+    console.log(req.params.name)
+    console.log(req.params.room)
 
     res.render('room', { roomId: req.params.room, name:req.params.name })
     
   })
+
   
   io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
+    socket.on('join-room', (roomId, userId,name) => {
+      p={ roomId, name }
+      x =users.push(p)
+
       socket.join(roomId)
       socket.to(roomId).broadcast.emit('user-connected', userId)
-  
+      io.to(roomId).emit("participants", name,users,roomId);
+      socket.on("message", (message) => {
+        io.to(roomId).emit("createMessage", message,name);
+      })
+     
       socket.on('disconnect', () => {
         socket.to(roomId).broadcast.emit('user-disconnected', userId)
       })
